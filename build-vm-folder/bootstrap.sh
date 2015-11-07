faf7f18247c4406b3af76300ca96bb408e46d79b
jentsan <jtsan@ncsu.edu>
2015-11-07 16:08:31 -0500

    Update bootstrap.sh

--- build-vm-folder/bootstrap.sh
+++ build-vm-folder/bootstrap.sh
@@ -1,5 +1,6 @@
-#ssh into the box
-vagrant ssh
-
 #Install Java
-wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh && bash equip_java8.sh
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh
+bash equip_java8.sh
+
+#Check Java version
+java -version
