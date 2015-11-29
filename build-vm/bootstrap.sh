e9d86202e9a113b2dab6002062c4262db4bbd315
jentsan <jtsan@ncsu.edu>
2015-11-28 21:17:59 -0500

    Update bootstrap.sh

--- build-vm/bootstrap.sh
+++ build-vm/bootstrap.sh
@@ -1,6 +1,95 @@
 #Install Java
+echo "Installing Java"
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh
-bash equip_java8.sh
+sudo bash equip_java8.sh
+rm equip_java8.sh
 
-#Check Java version
-java -version
+#Download ShopReview code
+echo "Downloading ShopReview code"
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0Bwnh9aHCFlSxTUc2OVhQZjFrN3c' -O ShopReview.zip
+unzip ShopReview.zip
+chmod -R 777 ShopReview
+mv ShopReview Desktop
+rm ShopReview.zip
+
+#Install Eclipse 
+echo "Installing eclipse"
+wget --no-check-certificate 'https://www.dropbox.com/s/dd627d3ujxg70b1/eclipse.tar.gz?dl=1' -O eclipse.tar.gz
+tar -xzvf eclipse.tar.gz
+mv eclipse Desktop
+rm eclipse.tar.gz
+
+#Download android-sdks
+echo "Downloading android-sdks"
+wget --no-check-certificate 'https://www.dropbox.com/s/b07oiozy2hkv038/android-sdks.tar.gz?dl=1' -O android-sdks.tar.gz
+echo "Extract android-sdks"
+tar -xzvf android-sdks.tar.gz
+rm android-sdks.tar.gz
+
+#Make Eclipse shortcut
+echo "Making an eclipse shortcut"
+#sudo touch ~/Desktop/Eclipse.desktop
+echo -e "[Desktop Entry]\nType=Application\nTerminal=false\nName=Eclipse\nIcon=Desktop/eclipse/icon.xpm\nExec=Desktop/eclipse/eclipse\nX-GNOME-Autostart-enabled=true" | sudo tee Desktop/Eclipse.desktop
+sudo chmod +x Desktop/Eclipse.desktop
+
+#Make Eclipse autostart
+mkdir home/vagrant/.config/autostart/
+echo "Making eclipse start on boot"
+#sudo touch home/vagrant/.config/autostart/Eclipse.desktop
+echo -e "[Desktop Entry]\nType=Application\nTerminal=false\nName=Eclipse\nIcon=Desktop/eclipse/icon.xpm\nExec=Desktop/eclipse/eclipse\nX-GNOME-Autostart-enabled=true" | sudo tee ~/.config/autostart/Eclipse.desktop
+sudo chmod +x home/vagrant/.config/autostart/Eclipse.desktop
+
+#Set launcher icons
+gsettings set com.canonical.Unity.Launcher favorites "['unity://running-apps','application://nautilus.desktop','unity://expo-icon','unity://devices']"
+
+#Download workspace
+echo "Downloading workspace"
+wget --no-check-certificate 'https://www.dropbox.com/s/0ao8gsqt4mzayak/workspace.tar.gz?dl=1' -O workspace.tar.gz
+echo "Extract workspace"
+tar -xzvf workspace.tar.gz
+rm workspace.tar.gz
+
+#Make the emulator run on 32bit
+echo -e "export ANDROID_EMULATOR_FORCE_32BIT=true" | sudo tee .profile -a
+
+#Download emulator
+echo "Downloading emulator"
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0Bwnh9aHCFlSxempGSmxMbEtJOUE' -O .android.zip
+unzip -o .android.zip
+rm .android.zip
+
+#Create emulator
+#echo "Rebooting"
+#sudo reboot
+#echo "Creating emulator"
+#sudo android-sdks/tools/android create avd -n AndroidDevice -t 2
+
+#Launch emulator on boot
+echo "Making emulator launch on boot"
+#sudo touch home/vagrant/.config/autostart/emulator.desktop
+echo -e "[Desktop Entry]\nType=Application\nTerminal=false\nName=emulator\nExec='android-sdks/tools/emulator -avd AndroidDevice'\nX-GNOME-Autostart-enabled=true" | sudo tee home/vagrant/.config/autostart/emulator.desktop
+sudo chmod +x home/vagrant/.config/autostart/emulator.desktop
+
+#Add Readme.txt
+echo "Downloading Readme.txt"
+wget https://github.com/SoftwareEngineeringToolDemos/FSE-2012-SelfMotion/raw/master/build-vm/Readme.txt
+echo "Moving Readme.txt to Desktop"
+mv Readme.txt Desktop
+
+#Add Instructions.txt
+echo "Downloading Insructions.txt"
+wget https://github.com/SoftwareEngineeringToolDemos/FSE-2012-SelfMotion/raw/master/build-vm/Instructions.txt
+echo "Moving Instructions.txt to Desktop"
+mv Instructions.txt Desktop
+
+#Add License.txt
+#wget
+#mv License.txt Desktop
+
+#Add Youtube Link - https://youtu.be/cVsFrIDQukY
+echo "Creating a youtube link"
+sudo touch ~/Desktop/SelfMotionDemo.desktop
+echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Link to SelfMotion Demo\nType=Link\nURL=https://youtu.be/cVsFrIDQukY\nIcon=text-html" | sudo tee Desktop/SelfMotionDemo.desktop
+
+#Shutdown
+sudo shutdown -h now 
